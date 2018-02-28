1cf18e47509fd27ea66765a71168dbb0c95ddc50
Paul Duguet <opauldo@gmail.com>
2018-02-28 20:45:08 +0000

    chore(debug): Added elm-live for live-debug

--- js/config.dev.js
+++ js/config.dev.js
@@ -2,7 +2,7 @@ var config = {
   offline: false,
   allowedIps: ['71.232.25.148', 'localhost'],
   scheme: 'http',
-  domain: '192.168.1.4',
+  domain: '192.168.15.9:8000',
   deezerAppId: '255142',
   spotifyClientId: '1d870e0c94764f68add93d63a41d2f5c'
 }
