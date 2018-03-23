9ef0fb601344f37d5fce39dc49af8a8da7cf30cb
Paul Duguet <opauldo@gmail.com>
2018-03-23 01:07:45 +0000

    Refactored SelectableList model

--- elm/SelectableList.elm
+++ elm/SelectableList.elm
@@ -2,39 +2,57 @@ module SelectableList exposing (SelectableList, fromList, toList, select, clear,
 
 
 type SelectableList a
-    = SelectableList (List a) (Maybe a)
+    = Selected (List a) a
+    | NotSelected (List a)
 
 
 fromList : List a -> SelectableList a
 fromList list =
-    SelectableList list Nothing
+    NotSelected list
 
 
 toList : SelectableList a -> List a
-toList (SelectableList list _) =
-    list
+toList sList =
+    case sList of
+        Selected list _ ->
+            list
+
+        NotSelected list ->
+            list
 
 
 select : a -> SelectableList a -> SelectableList a
-select el (SelectableList l _) =
-    if List.member el l then
-        SelectableList l (Just el)
-    else
-        SelectableList l Nothing
+select el sList =
+    let
+        list =
+            toList sList
+    in
+        if List.member el list then
+            Selected list el
+        else
+            NotSelected list
 
 
 clear : SelectableList a -> SelectableList a
-clear (SelectableList l _) =
-    SelectableList l Nothing
+clear sList =
+    NotSelected (toList sList)
 
 
 selected : SelectableList a -> Maybe a
-selected (SelectableList _ selected) =
-    selected
+selected sList =
+    case sList of
+        Selected _ el ->
+            Just el
+
+        NotSelected _ ->
+            Nothing
 
 
 map : (a -> b) -> SelectableList a -> SelectableList b
-map f (SelectableList l selected) =
-    selected
-        |> Maybe.map f
-        |> SelectableList (List.map f l)
+map f sList =
+    case sList of
+        Selected list el ->
+            Selected (List.map f list) (f el)
+
+        NotSelected list ->
+            NotSelected <| List.map f list
