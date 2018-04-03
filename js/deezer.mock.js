54688ebc66c922522aa0f53877f53f68558a640e
Paul Duguet <opauldo@gmail.com>
2018-04-03 22:46:14 +0100

    Fixed data format

--- js/deezer.mock.js
+++ js/deezer.mock.js
@@ -14,20 +14,26 @@ var DZ = {
   },
   api: function(url, cb) {
     if (url.startsWith("/user/me/playlists")) {
-      return [
-        { id: 1, title: "Favorites", nb_tracks: 5 },
-        { id: 2, title: "Fiesta", nb_tracks: 5 },
-        { id: 3, title: "Au calme", nb_tracks: 5 },
-        { id: 4, title: "Chouchou", nb_tracks: 5 }
-      ];
+      cb({
+        data: [
+          { id: 1, title: "Favorites", nb_tracks: 5 },
+          { id: 2, title: "Fiesta", nb_tracks: 5 },
+          { id: 3, title: "Au calme", nb_tracks: 5 },
+          { id: 4, title: "Chouchou", nb_tracks: 5 }
+        ]
+      });
     } else if (url.startsWith("/playlist/")) {
-      return [
-        { title: "I love you", artist: "Johnny Halliday" },
-        { title: "Don't forget me", artist: "Serge Gainsbourg" },
-        { title: "Take that", artist: "The Beatles" },
-        { title: "Bonjour la France", artist: "Peter McCalloway" },
-        { title: "Manger du pain", artist: "Petit Bourguignon" }
-      ];
+      cb({
+        tracks: {
+          data: [
+            { title: "I love you", artist: "Johnny Halliday" },
+            { title: "Don't forget me", artist: "Serge Gainsbourg" },
+            { title: "Take that", artist: "The Beatles" },
+            { title: "Bonjour la France", artist: "Peter McCalloway" },
+            { title: "Manger du pain", artist: "Petit Bourguignon" }
+          ]
+        }
+      });
     }
   }
 };
