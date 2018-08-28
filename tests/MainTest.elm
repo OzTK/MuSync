3f7e19bf676217a067b5cf6b656cee6ad937ec0f
Paul Duguet <opauldo@gmail.com>
2018-08-28 22:54:08 +0100

    Removed unnecessary tests

--- tests/MainTest.elm
+++
@@ -1,9 +0,0 @@
-module MainTest exposing (suite)
-
-import Test exposing (..)
-import Expect
-
-
-suite : Test
-suite =
-    describe "Project's initialization" [ test "succeeds" <| \_ -> Expect.pass ]
