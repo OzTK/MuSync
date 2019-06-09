2626bad8f97cfed347a013e2b6a8d571e23de312
Paul Duguet <opauldo@gmail.com>
2019-06-09 18:44:14 +0100

    Use new domain

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ var config = {
   offline: false,
   local: false,
   allowedIps: ["86.6.108.70", "185.78.8.131"],
-  scheme: "https",
-  domain: "playlist-importer.surge.sh",
+  scheme: "http",
+  domain: "playlist-importer.uk",
   deezerAppId: "254002",
   spotifyClientId: "1d870e0c94764f68add93d63a41d2f5c"
 };
