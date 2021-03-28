10d0974446de63ba21b533f143e559301084bc35
OzTK <OzTK@users.noreply.github.com>
2021-03-28 00:37:04 +0000

    deploy: b3f5221bb638accdd931f1f9ba09b6384ae402cd

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ var config = {
   offline: false,
   local: false,
   allowedIps: ["86.6.108.70", "185.78.8.131"],
-  scheme: "http",
-  domain: "musync.co.uk",
+  scheme: "https",
+  domain: "www.musync.co.uk",
   deezerAppId: "254002",
   spotifyClientId: "1d870e0c94764f68add93d63a41d2f5c",
   youtubeClientId:
