e55b989201ca9b24219560e13d4666b57880c77f
OzTK <OzTK@users.noreply.github.com>
2021-03-28 00:25:51 +0000

    deploy: b5fb431ba97039f4065a913068e90b58cfc2a4ea

---
+++ it/js/config.ci.js
@@ -0,0 +1,9 @@
+var config = {
+  offline: false,
+  local: true,
+  allowedIps: ["localhost"],
+  scheme: "http",
+  domain: "localhost:8000",
+  deezerAppId: "255142",
+  spotifyClientId: "1d870e0c94764f68add93d63a41d2f5c"
+};
---
+++ it/js/config.js
@@ -0,0 +1,11 @@
+var config = {
+  offline: false,
+  local: false,
+  allowedIps: ["86.6.108.70", "185.78.8.131"],
+  scheme: "http",
+  domain: "musync.co.uk",
+  deezerAppId: "254002",
+  spotifyClientId: "1d870e0c94764f68add93d63a41d2f5c",
+  youtubeClientId:
+    "934502674716-uasp2upchs0qbccd3j5v15jm8s74dedu.apps.googleusercontent.com",
+};
