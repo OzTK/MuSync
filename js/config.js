29b4d2c1bccab624cb62b8c79b27ef5a16d45baf
Paul Duguet <opauldo@gmail.com>
2023-11-22 23:25:07 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var config = {
   offline: false,
   local: false,
-  allowedIps: ["86.6.108.70", "185.78.8.131"],
+  allowedIps: ["101.188.67.134"],
   scheme: "https",
-  domain: "www.musync.co.uk",
+  domain: "oztk.github.io",
   deezerAppId: "254002",
   spotifyClientId: "1d870e0c94764f68add93d63a41d2f5c",
   youtubeClientId:
